74fbf4a70361674c936dbe2800a7ae262e0eb602
unknown <majdi.zourrig@myu.universitecentrale.tn>
2021-11-19 20:43:05 +0100

    modif code

--- src/Controller/TicketController.php
+++ src/Controller/TicketController.php
@@ -33,16 +33,15 @@ class TicketController extends AbstractController
         $ticket->setDescription($description);
         $ticket->setStatut("EN ATTENTE");
 
-//        $datenow = new \DateTime('now');
-//        echo $datenow;
+
 
                 $today = getdate();
         $ticket->setDate($today['mday'].'/'.$today['mon'].'/'.$today['year']);
 //
-        //Persister ajouter dans la transaction
+
         $manager->persist($ticket);
 //
-        //executer la transaction
+
         $manager->flush();
         $this->addFlash('success', "Ticket ADDED");
         return $this->render('ticket/index.html.twig', [
@@ -73,9 +72,9 @@ class TicketController extends AbstractController
     #[Route('/delete/{id}', name: 'ticket.delete')]
     public function deleteTicket(Ticket $ticket = null) {
         if($ticket) {
-            // recupérer manager
+
             $manager = $this->getDoctrine()->getManager();
-            // supprime le user avec le id
+
             $manager->remove($ticket);
             $manager->flush();
             $this->addFlash('success', "Ticket DELETED");
@@ -109,11 +108,11 @@ class TicketController extends AbstractController
         $limit = 3;
         $offset = ($nbPage-1)*$limit;
 
-        // Récupérer la liste des tickets
+
         $repository = $this->getDoctrine()->getRepository(ticket::class);
 
         $tickets = $repository->findBy([],[],$limit,$offset);
-        // l'envoyer à twig
+
 
         return $this->render('ticket/list.html.twig', [
             'tickets' => $tickets,
@@ -124,29 +123,10 @@ class TicketController extends AbstractController
 
     #[Route('/interval/date/{min}/{max}', name: 'ticket.list.date.interval')]
     public function listAllTicketsByIntervalDate(TicketRepository $ticketRepository, $min,$max) {
-//        $limit = 3;
-//        $offset = ($nbPage-1)*$limit;
-        //$tickets = new Ticket();
-       // $ticketParDate = [];
+//
         $tickets=$ticketRepository->TicketByDate($min,$max);
 
-//         Récupérer la liste des tickets
-//        $repository = $this->getDoctrine()->getRepository(ticket::class);
-//        $datemin = new DateTime($min);
-//        $datemax = new DateTime($max);
-//        $tickets = $repository->
 //
-////        for ($i=0;$i<=$tickets<>Null;$i++) {
-////
-////
-////            $datecreation = new \DateTime($tickets->date);
-////            if ($datecreation<=$datemax and $datecreation>=$datemin)
-////            {
-////                $ticketParDate[]=$tickets;
-////            }
-////        }
-//        // l'envoyer à twig
-
         return $this->render('ticket/list-par-interv-date.html.twig', [
             'tickets' => $tickets
         ]);
